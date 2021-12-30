8582e43baa1368e88d40492fd12fd8461075736f
hung <buihungtpd3@gmail.com>
2021-12-30 23:22:24 +0700

    Update images

--- src/home/views.py
+++ src/home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .forms import InputForm
 from django.http import HttpResponseRedirect
 from .PoSTagger import renderTreee
+
+import os
+from syntaxTree.settings import MEDIA_ROOT, MEDIA_URL, STATIC_URL, BASE_DIR
 # Create your views here.
 
 def home (request):
@@ -10,5 +13,10 @@ def home (request):
 def renderTree(request):
     sentence = request.POST['name']
     sentence = renderTreee(sentence)
-    sentence = "Your tree here!!"
-    return render(request, 'result.html',{'result': sentence})
+    sentence = "Your tree here!!!"
+
+    img_list = os.listdir(os.path.join(BASE_DIR) + "\\home\\static\\images") 
+    print(img_list)
+
+    context = {'result' : sentence, 'imgs': img_list}
+    return render(request, 'result.html', context)
